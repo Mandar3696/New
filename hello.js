4f8fc779ccc99c3fdabaf57d02c1a590de85c4e4
Mandar Masal <mandarmasal3696@gmail.com>
2025-06-05 13:04:23 +0530

    learning

---
+++ hello.js
@@ -0,0 +1,34 @@
+console.log("Hello, World!");
+let fil = 15;
+fil='string';
+fil= 18;
+console.log(fil);
+
+const bal = 17;
+console.log(bal);
+
+
+let num1 = 4;
+
+if (num1 % 2 == 0)
+{
+    console.log("Even");
+}
+else
+{
+    console.log("Odd");
+}
+
+
+let num2 = 5;
+if (num2 % 2 == 0)
+{
+    console.log("Even");
+}else
+{
+    console.log("Odd");
+}
+
+
+
+
